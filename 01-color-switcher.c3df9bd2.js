1b2f891359930a0173b16bc5733689c6d46391b6
tizzifona <100047250+tizzifona@users.noreply.github.com>
2023-03-28 23:55:51 +0000

    Deploying to gh-pages from @ tizzifona/goit-js-hw-09@45d310543dbdf34e2ecee1575d0dab2c7f2e82d8 🚀

---
+++ 01-color-switcher.c3df9bd2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;function o(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),n=setInterval((function(){o()}),1e3),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}))}))}();
+//# sourceMappingURL=01-color-switcher.c3df9bd2.js.map
---
+++ 01-color-switcher.ccdea070.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;function d(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(()=>{t.setAttribute("disabled","disabled"),o=setInterval((()=>{d()}),1e3),e.addEventListener("click",(()=>{clearInterval(o),t.removeAttribute("disabled")}))}));
+//# sourceMappingURL=01-color-switcher.ccdea070.js.map
